51611d5a0093c4b3b1f2475b38e83b5d86f6b8a4
Ignacy P. Biernacki <ignac.biernacki@gmail.com>
2022-06-16 15:52:23 +0200

    pwsh & ohmyposh

---
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,21 @@
+if (!(Get-InstalledModule Terminal-Icons)) 
+{
+    Install-Module -Name Terminal-Icons -Repository PSGallery -Force
+}
+
+
+if (!(Get-InstalledModule PSReadLine))
+{
+    Install-Module PSReadLine -AllowPrerelease -Force -AcceptLicense
+}
+
+
+Import-Module -Name Terminal-Icons
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineKeyHandler -Chord Shift+Spacebar -Function MenuComplete
+
+
+oh-my-posh --init --shell pwsh --config ~/.oh-my-posh | Invoke-Expression
